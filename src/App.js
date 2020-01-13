249ee870926fd2eda0cfc0fc8080c5e43d38fddd
vamsi <vamsikrishna3415@outlook.in>
2020-01-13 21:18:57 +0530

    added context providers

--- src/App.js
+++ src/App.js
@@ -3,24 +3,29 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ReactNavbar from "./components/Layout/ReactNavbar";
 import Home from "./components/pages/Home";
 import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import "./App.css";
+import Alerts from "./components/Layout/Alerts";
 const App = () => {
   return (
     <AuthState>
-      <Router>
-        <Fragment>
-          <ReactNavbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/signup' component={Register} />
-              <Route exact path='/login' component={Login} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
+      <AlertState>
+        <Router>
+          <Fragment>
+            <ReactNavbar />
+            <div className='container'>
+              <Alerts />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/signup' component={Register} />
+                <Route exact path='/login' component={Login} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </AlertState>
     </AuthState>
   );
 };
