8505baf79fa46a9219078a70d09b0d05d85c65ee
vamsi <vamsikrishna3415@outlook.in>
2020-01-13 21:20:40 +0530

    added all types here

--- src/context/types.js
+++ src/context/types.js
@@ -5,3 +5,5 @@ export const AUTH_ERROR = "AUTH_ERROR";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
+export const SET_ALERT = "SET_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT";
