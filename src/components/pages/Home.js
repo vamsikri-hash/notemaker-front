a3fbefbb7dfa47a1ebfb60e5f0888109656bd9ad
vamsi <vamsikrishna3415@outlook.in>
2020-01-14 15:14:44 +0530

    added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import AuthContext from "../../context/auth/authContext";
 
 const Home = () => {
+  const authContext = useContext(AuthContext);
+  useEffect(() => {
+    authContext.loaduser();
+    //eslint-disable-next-line
+  }, []);
   return <div>Hello</div>;
 };
 
