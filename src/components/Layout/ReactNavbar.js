21d0797703d0d0517214d67d6034017a75897cc3
vamsi <vamsikrishna3415@outlook.in>
2020-01-14 15:14:01 +0530

    separated auth and guest links

--- src/components/Layout/ReactNavbar.js
+++ src/components/Layout/ReactNavbar.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { Fragment, useContext } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import AuthContext from "../../context/auth/authContext";
 
 const ReactNavbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+  const { isAuthenticated, logout, user } = authContext;
+
+  const onLogout = () => {
+    logout();
+  };
+  const authlinks = (
+    <Fragment>
+      <li>hello {user && user.name}</li>
+      <li>
+        <a onClick={onLogout} href='/'>
+          Logout
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestlinks = (
+    <Fragment>
+      <li>
+        <Link to='/signup'>Signup</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </Fragment>
+  );
+
   return (
     <div className='navbar bg-primary'>
       <h1>{title}</h1>
 
-      <ul>
-        <li>
-          <Link to='/signup'>Signup</Link>
-        </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-      </ul>
+      <ul>{isAuthenticated ? authlinks : guestlinks}</ul>
     </div>
   );
 };
