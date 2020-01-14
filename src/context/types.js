a6ddf46659070a71e03e40b2aa1f0f94a77bf6e3
vamsi <vamsikrishna3415@outlook.in>
2020-01-14 15:13:20 +0530

    added LOGOUT

--- src/context/types.js
+++ src/context/types.js
@@ -5,5 +5,6 @@ export const AUTH_ERROR = "AUTH_ERROR";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
+export const LOGOUT = "LOGOUT";
 export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
