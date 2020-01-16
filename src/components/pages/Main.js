2656c11c196c4b877f699db0b55c9b1d415f74ab
vamsi <vamsikrishna3415@outlook.in>
2020-01-16 15:53:03 +0530

    dummu page

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,7 +1,18 @@
 import React from "react";
+import NoteForm from "../notes/NoteForm";
+import Notes from "../notes/Notes";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <div className='grid-2'>
+      <div>
+        <NoteForm />
+      </div>
+      <div>
+        <Notes />
+      </div>
+    </div>
+  );
 };
 
 export default Main;
