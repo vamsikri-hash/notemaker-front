b4d4ad7df4c05ab2885bf46170031d6b2deea5fb
vamsi <vamsikrishna3415@outlook.in>
2020-01-28 20:33:52 +0530

    added item context

---
+++ src/context/item/ItemState.js
@@ -0,0 +1,145 @@
+import React, { useReducer } from "react";
+import axios from "axios";
+import ItemContext from "./itemContext";
+import itemReducer from "./itemReducer";
+import {
+  GET_ITEMS,
+  ADD_ITEM,
+  DELETE_ITEM,
+  SET_CURRENT_ITEM,
+  CLEAR_CURRENT_ITEM,
+  UPDATE_ITEM,
+  CLEAR_ITEMS,
+  ITEM_ERROR,
+  CLEAR_ITEM_ERROR
+} from "../types";
+
+const ItemState = props => {
+  const initialState = {
+    items: [],
+    current: null,
+    error: null
+  };
+  const [state, dispatch] = useReducer(itemReducer, initialState);
+
+  //Get Items
+  const GetItems = async () => {
+    const config = {
+      headers: {
+        "Content-Type": "Application/json"
+      }
+    };
+    try {
+      const res = await axios.get(
+        "http://localhost:3000/api/v1/notes",
+
+        config
+      );
+      console.log(res.data);
+      dispatch({
+        type: GET_ITEMS,
+        payload: res.data
+      });
+    } catch (error) {
+      console.log(error.response);
+      dispatch({ type: ITEM_ERROR, payload: error.response.data.message });
+    }
+  };
+  //Add Item
+  const AddItem = async obj => {
+    const config = {
+      headers: {
+        "Content-Type": "Application/json"
+      }
+    };
+    try {
+      console.log(obj);
+      const res = await axios.post(
+        "http://localhost:3000/api/v1/notes",
+        obj,
+        config
+      );
+      console.log(res.data);
+      dispatch({
+        type: ADD_ITEM,
+        payload: res.data
+      });
+    } catch (error) {
+      console.log(error.response);
+      dispatch({ type: ITEM_ERROR, payload: error.response.data.message });
+    }
+  };
+
+  //Update Item
+  const UpdateItem = async obj => {
+    const config = {
+      headers: {
+        "Content-Type": "Application/json"
+      }
+    };
+    try {
+      const res = await axios.put(
+        `http://localhost:3000/api/v1/notes/${obj.id}`,
+        obj,
+        config
+      );
+
+      dispatch({
+        type: UPDATE_ITEM,
+        payload: res.data
+      });
+      console.log(state.items);
+    } catch (error) {
+      console.log(error.response);
+      dispatch({ type: ITEM_ERROR, payload: error.response.data.message });
+    }
+  };
+  //DeleteItem
+
+  const DeleteItem = async id => {
+    try {
+      await axios.delete(`http://localhost:3000/api/v1/notes/${id}`);
+      dispatch({
+        type: DELETE_ITEM,
+        payload: id
+      });
+    } catch (error) {
+      dispatch({ type: ITEM_ERROR, payload: error.response.data });
+    }
+  };
+
+  //setCurrent
+  const setCurrentItem = item => {
+    dispatch({ type: SET_CURRENT_ITEM, payload: item });
+  };
+  //Clear Current
+  const clearCurrentItem = () => {
+    dispatch({ type: CLEAR_CURRENT_ITEM });
+  };
+  //clear Item Error
+  const clearItemError = () => {
+    dispatch({ type: CLEAR_ITEM_ERROR });
+  };
+  //Clear Item
+
+  return (
+    <ItemContext.Provider
+      value={{
+        items: state.items,
+        current: state.current,
+        error: state.error,
+        AddItem,
+        GetItems,
+        UpdateItem,
+        DeleteItem,
+        setCurrentItem,
+        clearCurrentItem,
+        clearItemError
+      }}
+    >
+      {props.children}
+    </ItemContext.Provider>
+  );
+};
+
+export default ItemState;
---
+++ src/context/item/itemReducer.js
@@ -0,0 +1,62 @@
+import {
+  GET_ITEMS,
+  ADD_ITEM,
+  DELETE_ITEM,
+  SET_CURRENT_ITEM,
+  CLEAR_CURRENT_ITEM,
+  UPDATE_ITEM,
+  CLEAR_ITEMS,
+  ITEM_ERROR,
+  CLEAR_ITEM_ERROR
+} from "../types";
+
+export default (state, action) => {
+  switch (action.type) {
+    case GET_ITEMS:
+      return {
+        ...state,
+        items: action.payload
+      };
+    case ADD_ITEM:
+      return {
+        ...state,
+        items: [action.payload, ...state.items]
+      };
+    case UPDATE_ITEM:
+      return {
+        ...state,
+        items: state.items.map(item =>
+          item.id === action.payload.id ? action.payload : item
+        )
+      };
+
+    case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter(item => item.id !== action.payload)
+      };
+    case SET_CURRENT_ITEM:
+      return {
+        ...state,
+        current: action.payload
+      };
+    case CLEAR_CURRENT_ITEM:
+      return {
+        ...state,
+        current: null
+      };
+    case ITEM_ERROR:
+      return {
+        ...state,
+        error: action.payload
+      };
+    case CLEAR_ITEM_ERROR:
+      return {
+        ...state,
+        current: null,
+        error: null
+      };
+    default:
+      return state;
+  }
+};
