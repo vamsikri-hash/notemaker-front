a848bdce2e7fb6ff5ff77d17675b43f6d3ae5eac
vamsi <vamsikrishna3415@outlook.in>
2020-01-29 18:19:23 +0530

    added types for Item Context

--- src/context/types.js
+++ src/context/types.js
@@ -8,6 +8,16 @@ export const CLEAR_NOTES = "CLEAR_NOTES";
 export const NOTE_ERROR = "NOTE_ERROR";
 export const CLEAR_NOTE_ERROR = "CLEAR_NOTE_ERROR";
 
+export const GET_ITEMS = "GET_ITEMS";
+export const ADD_ITEM = "ADD_ITEM";
+export const DELETE_ITEM = "DELETE_ITEM";
+export const SET_CURRENT_ITEM = "SET_CURRENT_ITEM";
+export const CLEAR_CURRENT_ITEM = "CLEAR_CURRENT_ITEM";
+export const UPDATE_ITEM = "UPDATE_ITEM";
+export const CLEAR_ITEMS = "CLEAR_ITEMS";
+export const ITEM_ERROR = "ITEM_ERROR";
+export const CLEAR_ITEM_ERROR = "CLEAR_ITEM_ERROR";
+
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
 export const USER_LOADED = "USER_LOADED";
