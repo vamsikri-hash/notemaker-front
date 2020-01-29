296730190f708b14b5ac5477bfcb2d1cd84066ae
vamsi <vamsikrishna3415@outlook.in>
2020-01-29 18:18:57 +0530

    none

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,6 +1,4 @@
 import React from "react";
-import NoteForm from "../notes/NoteForm";
-import Notes from "../notes/Notes";
 
 const Main = () => {
   return <div className='grid-2'>Hello Busy People</div>;
